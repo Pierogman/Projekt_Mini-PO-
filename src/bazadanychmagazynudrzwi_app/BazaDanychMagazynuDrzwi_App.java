95bf0efd3739afff693b023722f6fd196f5514c7
tymku <tymku@Vivobook_Tymek>
2025-05-15 14:32:11 +0200

    Pierwszy commit!

---
+++ src/bazadanychmagazynudrzwi_app/BazaDanychMagazynuDrzwi_App.java
@@ -0,0 +1,13 @@
+package bazadanychmagazynudrzwi_app;
+
+//To jest główna klasa tego porgramu. Z tego miejsca będą inicjowane pozostałe
+//części programu. 
+
+public class BazaDanychMagazynuDrzwi_App
+{
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
