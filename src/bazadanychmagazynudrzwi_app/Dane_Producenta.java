03464944d7b0b5b27b9dcc1c10f975a79af45f50
tymku <tymku@Vivobook_Tymek>
2025-05-16 15:17:46 +0200

    Dodanie pełniej klasy Dane_Producenta

---
+++ src/bazadanychmagazynudrzwi_app/Dane_Producenta.java
@@ -0,0 +1,37 @@
+package bazadanychmagazynudrzwi_app;
+
+/**
+ * To jest klasa przechowywująca informacje od producenta
+ *
+ * @author tymku
+ */
+public class Dane_Producenta
+{
+
+    private final String nazwa_producenta;
+    private final String nazwa_produktu;
+
+    public Dane_Producenta(String nazwa_producenta, String nazwa_produktu)
+    {
+        this.nazwa_producenta = nazwa_producenta;
+        this.nazwa_produktu = nazwa_produktu;
+    }
+
+    public Dane_Producenta(Dane_Producenta dane_producenta)
+    {
+        this.nazwa_producenta = dane_producenta.nazwa_producenta;
+        this.nazwa_produktu = dane_producenta.nazwa_produktu;
+    }
+
+    @Override
+    public String toString()
+    {
+        return nazwa_producenta + ";" + nazwa_produktu;
+    }
+
+    public boolean equals(Dane_Producenta dane_producenta)
+    {
+        return this.nazwa_producenta.equals(dane_producenta.nazwa_producenta) &&
+                this.nazwa_produktu.equals(dane_producenta.nazwa_produktu);
+    }
+}
