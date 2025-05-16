98176c6179dd90c7b4aaca72884c7dabed875160
tymku <tymku@Vivobook_Tymek>
2025-05-16 15:16:23 +0200

    Pierwszy commit!

---
+++ src/bazadanychmagazynudrzwi_app/Produkt.java
@@ -0,0 +1,35 @@
+package bazadanychmagazynudrzwi_app;
+
+/**
+ * To jest klasa abstrakcyja z której kasy Drzwi, Klamki, Ościerznice
+ *
+ * @author tymku
+ */
+public abstract class Produkt
+{
+    protected int typ;
+    protected int numer_ID;
+    protected int numer_WZ;
+    protected Dane_Producenta dane_producenta;
+
+    // Generowanie id na podstawie ilosci produktu
+    protected void generuj_ID(int typ, int numer)
+    {
+        this.numer_ID = 100000 * typ + numer;
+    }
+
+    // Uzupełanie danych na podstawie podanego id 
+    protected void generuj_ID(int id)
+    {
+        this.numer_ID = id;
+    }
+
+    public int usun()
+    {
+        int urzywane_id = this.numer_ID;
+        numer_ID = 0;
+        numer_WZ = 0;
+
+        return urzywane_id;
+    }
+}
