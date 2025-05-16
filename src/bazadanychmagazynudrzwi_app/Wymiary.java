382a8f8e329fd265dce8c25ef529601a5a2ad165
tymku <tymku@Vivobook_Tymek>
2025-05-16 15:17:23 +0200

    Dodanie pełniej klasy Wymiary

---
+++ src/bazadanychmagazynudrzwi_app/Wymiary.java
@@ -0,0 +1,40 @@
+package bazadanychmagazynudrzwi_app;
+
+/**
+ * To jest klasa przechowywująca wymiary damnego produktu
+ *
+ * @author tymku
+ */
+public class Wymiary
+{
+
+    private final double szerokosc;
+    private final double wysokosc;
+    private final double grubosc;
+
+    public Wymiary(double szerokosc, double wysokosc, double grubosc)
+    {
+        this.szerokosc = szerokosc;
+        this.wysokosc = wysokosc;
+        this.grubosc = grubosc;
+    }
+
+    public Wymiary(Wymiary wymiary)
+    {
+        this.szerokosc = wymiary.szerokosc;
+        this.wysokosc = wymiary.wysokosc;
+        this.grubosc = wymiary.grubosc;
+    }
+
+    @Override
+    public String toString()
+    {
+        return szerokosc + " x " + wysokosc + " x " + grubosc + " x ";
+    }
+
+    public boolean equals(Wymiary wymiary)
+    {
+        return !((this.szerokosc != wymiary.szerokosc || this.wysokosc != wymiary.wysokosc)
+                || this.grubosc != wymiary.grubosc);
+    }
+}
