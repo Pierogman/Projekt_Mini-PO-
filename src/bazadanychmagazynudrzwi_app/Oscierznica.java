7de815db01db6db363fe81240394bc9c74dcfdcb
tymku <tymku@Vivobook_Tymek>
2025-05-18 18:12:35 +0200

    Zmiany 18.05

---
+++ src/bazadanychmagazynudrzwi_app/Oscierznica.java
@@ -0,0 +1,62 @@
+package bazadanychmagazynudrzwi_app;
+
+/**
+ *
+ * @author tymku
+ */
+public class Oscierznica extends Produkt
+{
+
+    private static int ilosc_stworzonych_oscerznic;
+
+    private Wymiary wymiary;
+    private String material;
+
+    public Oscierznica(int id, int numer_WZ, Dane_Producenta dane_producenta, Wymiary wymiary, String material)
+    {
+        ilosc_stworzonych_oscerznic++;
+
+        super.typ = 3;
+        super.numer_PZ = numer_WZ;
+        super.generuj_ID(id);
+
+        super.dane_producenta = new Dane_Producenta(dane_producenta);
+
+        this.wymiary = new Wymiary(wymiary);
+        this.material = material;
+    }
+
+    public Oscierznica(int numer_WZ, Dane_Producenta dane_producenta, Wymiary wymiary, String material)
+    {
+        ilosc_stworzonych_oscerznic++;
+
+        super.typ = 3;
+        super.numer_PZ = numer_WZ;
+        super.generuj_ID(typ, ilosc_stworzonych_oscerznic - 1);
+
+        super.dane_producenta = new Dane_Producenta(dane_producenta);
+
+        this.wymiary = new Wymiary(wymiary);
+        this.material = material;
+    }
+
+    // Metody: 
+    public boolean compareTo(Wymiary wymiary)
+    {
+        return this.wymiary.compareTo(wymiary);
+    }
+
+    public String toFormatedString(String format)
+    {
+        return String.format(format, super.numer_ID, super.dane_producenta.toString(), wymiary.toString(), material, " ");
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.numer_ID + ";" + super.dane_producenta.formatuj_do_zapisu() + ";"
+                + wymiary.formatuj_do_zapisu() + ";" + material + ";" + " ";
+
+    }
+
+}
