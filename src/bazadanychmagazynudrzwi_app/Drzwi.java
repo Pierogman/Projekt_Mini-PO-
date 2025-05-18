0e434cb97009dbb7c3b899a806ce6b6f2d7d6298
tymku <tymku@Vivobook_Tymek>
2025-05-18 18:10:20 +0200

    Zmiany 18.05

--- src/bazadanychmagazynudrzwi_app/Drzwi.java
+++ src/bazadanychmagazynudrzwi_app/Drzwi.java
@@ -15,11 +15,12 @@ public class Drzwi extends Produkt
     private int ilosc_skrzydel;
 
     // Konstruktory:
-    public Drzwi(int id, Wymiary wymiary, String material, int ilosc_skrzydel, Dane_Producenta dane_producenta)
+    public Drzwi(int id, int nume_WZ, Dane_Producenta dane_producenta, Wymiary wymiary, String material, int ilosc_skrzydel)
     {
         ilosc_stworzonych_drzwi++;
 
         super.typ = 1;
+        super.numer_PZ = numer_PZ;
         super.generuj_ID(id);
 
         super.dane_producenta = new Dane_Producenta(dane_producenta);
@@ -29,12 +30,13 @@ public class Drzwi extends Produkt
         this.ilosc_skrzydel = ilosc_skrzydel;
     }
 
-    public Drzwi(Wymiary wymiary, String material, int ilosc_skrzydel, Dane_Producenta dane_producenta)
+    public Drzwi(int numer_WZ, Dane_Producenta dane_producenta, Wymiary wymiary, String material, int ilosc_skrzydel)
     {
         ilosc_stworzonych_drzwi++;
 
         super.typ = 1;
-        super.generuj_ID(ilosc_stworzonych_drzwi - 1, typ);
+        super.numer_PZ = numer_WZ;
+        super.generuj_ID(typ, ilosc_stworzonych_drzwi - 1);
 
         super.dane_producenta = new Dane_Producenta(dane_producenta);
 
@@ -46,7 +48,22 @@ public class Drzwi extends Produkt
     //Metody:
     public boolean compareTo(Wymiary wymiary)
     {
-        return this.wymiary.equals(wymiary);
+        return this.wymiary.compareTo(wymiary);
+    }
+
+    public String toFormatedString(String format)
+
+    {
+        return String.format(format,
+                super.numer_ID, super.dane_producenta.toString(), wymiary.toString(), material, ilosc_skrzydel);
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.numer_ID + ";" + super.dane_producenta.formatuj_do_zapisu() + ";"
+                + wymiary.formatuj_do_zapisu() + ";" + material + ";" + ilosc_skrzydel;
+
     }
 
 }
