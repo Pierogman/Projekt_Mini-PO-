e5b349af90e2365097d18af0c2d91ff19bf95c19
tymku <tymku@Vivobook_Tymek>
2025-05-19 15:16:22 +0200

    Zmiany 19.05

--- src/bazadanychmagazynudrzwi_app/Oscierznica.java
+++ src/bazadanychmagazynudrzwi_app/Oscierznica.java
@@ -40,23 +40,46 @@ public class Oscierznica extends Produkt
         this.material = material;
     }
 
+    public Oscierznica(String dane, boolean dodaj_ID)
+    {
+
+        String[] osobne_dane = dane.split(";");
+
+        if (!dodaj_ID)
+        {
+            super.generuj_ID(Integer.parseInt(osobne_dane[0]));
+        } else
+        {
+            ilosc_stworzonych_oscerznic++;
+            super.generuj_ID(typ, ilosc_stworzonych_oscerznic - 1);
+        }
+
+        super.typ = 1;
+        super.numer_PZ = Integer.parseInt(osobne_dane[1]);
+        super.dane_producenta = new Dane_Producenta(osobne_dane[2], osobne_dane[3]);
+
+        this.wymiary = new Wymiary(Double.parseDouble(osobne_dane[4]), Double.parseDouble(osobne_dane[5]), Double.parseDouble(osobne_dane[6]));
+        this.material = osobne_dane[7];
+    }
+
     // Metody: 
     public boolean compareTo(Wymiary wymiary)
     {
         return this.wymiary.compareTo(wymiary);
     }
 
+    public boolean compareTo(Oscierznica oscierznica)
+    {
+        return super.dane_producenta.compareTo(oscierznica.dane_producenta)
+                && this.wymiary.compareTo(oscierznica.wymiary) && this.material.equals(oscierznica.material);
+    }
+
     public String toFormatedString(String format)
     {
-        return String.format(format, super.numer_ID, super.dane_producenta.toString(), wymiary.toString(), material, " ");
+         return String.format(format,
+                super.numer_ID, super.numer_PZ, super.dane_producenta.formatuj_do_zapisu()
+                , wymiary.formatuj_do_zapisu(), material, " ");  
     }
 
-    @Override
-    public String toString()
-    {
-        return super.numer_ID + ";" + super.dane_producenta.formatuj_do_zapisu() + ";"
-                + wymiary.formatuj_do_zapisu() + ";" + material + ";" + " ";
-
-    }
-
+    
 }
