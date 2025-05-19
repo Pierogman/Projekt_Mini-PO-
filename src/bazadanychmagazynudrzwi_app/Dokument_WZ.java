ffe7524be2eb7f1eff251914b492be6bba60eda2
tymku <tymku@Vivobook_Tymek>
2025-05-19 20:31:16 +0200

    Zmiany 19.05

---
+++ src/bazadanychmagazynudrzwi_app/Dokument_WZ.java
@@ -0,0 +1,46 @@
+package bazadanychmagazynudrzwi_app;
+
+/**
+ *
+ * @author ACER Nitro5
+ */
+public class Dokument_WZ extends Dokument
+{
+
+    private static int liczba_stworzonych_WZ = 0;
+
+    // Konstruktor z nadanym ID
+    public Dokument_WZ(int id, Pracownik p, String firma, String sciezka)
+    {
+        super(id, 1, p, firma, sciezka);
+    }
+    
+    public Dokument_WZ(int id, Dokument_WZ dokument_wz)
+    {
+        super(id, 1, dokument_wz.pracownik, dokument_wz.nazwa_firmy, dokument_wz.sciezka_dokumentu);
+    }
+
+    // Konstruktor z automatycznym ID
+    public Dokument_WZ(Pracownik p, String firma, String sciezka)
+    {
+        super(0, 1, p, firma, sciezka);
+        generuj_ID(1, liczba_stworzonych_WZ++);
+    }
+
+    // Konstruktor z linii tekstu
+    public Dokument_WZ(String linia)
+    {
+        super(linia, 1);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "WZ #" + daneBazoweDoZapisu();
+    }
+
+    public String formatuj_do_zapisu()
+    {
+        return daneBazoweDoZapisu();
+    }
+}
